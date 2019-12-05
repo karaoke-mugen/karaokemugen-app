13068c1cf964d0d64185d6d9c4532c3cc3158f3b
Guillaume Lebigot <axel@teri-chan.net>
2019-12-05 16:43:36 +0100

    Made something that works with Electron.

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,9 @@ import {createInterface} from 'readline';
 import { setState, getState } from './utils/state';
 import { version } from './version';
 import { getPortPromise } from 'portfinder';
+import { app, BrowserWindow } from 'electron';
+
+console.log(app);
 
 process.on('uncaughtException', exception => {
 	console.log('Uncaught exception:', exception);
@@ -73,12 +76,67 @@ setState({appPath: appPath, dataPath: dataPath});
 
 process.env['NODE_ENV'] = 'production'; // Default
 
-main()
-	.catch(err => {
-		logger.error(`[Launcher] Error during launch : ${err}`);
-		console.log(err);
-		exit(1);
+let win: any;
+
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+app.on('ready', () => {
+	createWindow();
+	main()
+		.catch(err => {
+			logger.error(`[Launcher] Error during launch : ${err}`);
+			console.log(err);
+			exit(1);
 	});
+});
+
+// Quitte l'application quand toutes les fenêtres sont fermées.
+app.on('window-all-closed', () => {
+	// Sur macOS, il est commun pour une application et leur barre de menu
+	// de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+	if (process.platform !== 'darwin') {
+	  app.quit();
+	}
+})
+
+
+app.on('activate', () => {
+	// Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+	// l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+	if (win === null) {
+	  createWindow();
+	}
+});
+
+
+
+function createWindow () {
+	// Cree la fenetre du navigateur.
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	// and load the index.html of the app.
+	win.loadFile(__dirname + 'index.html');
+
+	// Ouvre les DevTools.
+	win.webContents.openDevTools()
+
+	// Émit lorsque la fenêtre est fermée.
+	win.on('closed', () => {
+	  // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
+	  // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
+	  // où vous devez supprimer l'élément correspondant.
+	  win = null
+	  exit(0);
+	})
+}
+
 
 async function main() {
 	const argv = minimist(process.argv.slice(2));
