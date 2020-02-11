b4c4e3caad636d2d2374448f983bf804bdbe3d33
Guillaume Lebigot <axel@teri-chan.net>
2020-02-11 12:42:35 +0100

    Removed afterPack (not used anymore)

--- afterPack.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-exports.default = context => {
-	fs.mkdirSync('dist/win-unpacked/app');
-	fs.mkdirSync('dist/win-unpacked/app/bin');
-	fs.copySync('app/bin', 'dist/win-unpacked/app/bin/');
-	fs.copySync('app/config.yml', 'dist/win-unpacked/config.yml');
-	fs.copySync('app/database.json', 'dist/win-unpacked/database.json');
-}
