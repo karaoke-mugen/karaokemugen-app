bedbfebcb4d7f7002595db8f3799a96bf3e2e3e4
Guillaume Lebigot <axel@teri-chan.net>
2020-02-12 23:11:17 +0100

    Remove useless ipcinit

--- src/utils/electron_logger.ts
+++ src/utils/electron_logger.ts
@@ -1,12 +1,6 @@
 import Transport from 'winston-transport';
 import {ipcMain} from 'electron';
 
-export function initIPC() {
-	ipcMain.on('message', (event, data) => {
-		event.sender.send('message', data);
-	});
-}
-
 export class IPCTransport extends Transport {
 	constructor(opts: any) {
 		super(opts);
