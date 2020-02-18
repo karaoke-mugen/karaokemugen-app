643946526ffe38f62562472a2abf7a267212dc9f
Aeden <florentius.b@gmail.com>
2020-02-18 22:40:44 +0100

    Add nanami #568

--- initpage/initpage.js
+++ initpage/initpage.js
@@ -1,7 +1,9 @@
 const ipcRenderer = require('electron').ipcRenderer
 ipcRenderer.on('initStep', (event, data)=> {
 	let div =document.getElementById('init');
-	div.innerText = div.innerText +'\n'+data
+	div.innerHTML = '<div>' + data + '</div>';
+	let nanamiSD =document.getElementById('nanamiSD');
+	nanamiSD.innerHTML = "<img class='nanamiSD' src='./public/nanami-hehe2.png' />";
 });
 ipcRenderer.on('log', (event, data)=> {
 	let div =document.getElementById('logs');
@@ -10,7 +12,9 @@ ipcRenderer.on('log', (event, data)=> {
 ipcRenderer.on('error', (event, data)=> {
 	if (!buttonLogsStatus) clickButton();
 	let div =document.getElementById('error');
-	div.innerHTML = "<div>" + data + "</div>"
+	div.innerHTML = "<div>" + data + "</div>";
+	let nanamiSD =document.getElementById('nanamiSD');
+	nanamiSD.innerHTML = "<img class='nanamiSD' src='./public/nanami-surpris.png' />";
 })
 
 let buttonLogsStatus = false;
