a2a46ba3d005e4f8f0e18e1cdd6c3f41a35e6906
Aeden <florentius.b@gmail.com>
2020-02-18 22:23:41 +0100

    maj init page

--- initpage/initpage.js
+++ initpage/initpage.js
@@ -1,6 +1,22 @@
 const ipcRenderer = require('electron').ipcRenderer
+ipcRenderer.on('initStep', (event, data)=> {
+	let div =document.getElementById('init');
+	div.innerText = div.innerText +'\n'+data
+});
 ipcRenderer.on('log', (event, data)=> {
-	console.log(data)
 	let div =document.getElementById('logs');
-	div.innerText = div.innerText +'\n'+data.message
+	div.innerHTML = div.innerHTML + "<div>" + data.message + "</div>"
 })
+ipcRenderer.on('error', (event, data)=> {
+	if (!buttonLogsStatus) clickButton();
+	let div =document.getElementById('error');
+	div.innerHTML = "<div>" + data + "</div>"
+})
+
+let buttonLogsStatus = false;
+document.getElementById("buttonLogs").onclick = clickButton;
+
+function clickButton () {
+	document.getElementById("logs").className = buttonLogsStatus ? 'hidden' : '';
+	buttonLogsStatus = !buttonLogsStatus;
+}
